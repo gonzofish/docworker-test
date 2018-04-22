efb8d352969b1498d44e4a5ac168a7ca60122f96
Matt Fehskens <matt.fehskens@gmail.com>
2018-04-21 22:34:36 -0400

    JSDocs for my-service

--- src/services/my-service.js
+++ src/services/my-service.js
@@ -1,19 +1,22 @@
-/**
- * @name myService.sayAge
- * @param {number} age - The age
- * @description Echo the age provided
- */
 const sayAge = (age) => sayYour('age', age);
-/**
- * Echo the name provided
- * @param {string} age - The name
- * @name myService.sayName
- */
 const sayName = (age) => sayYour('name', name);
 
 const sayYours = (type, value) => console.info(`Your ${type} is ${value}!`);
 
+/**
+ * @namespace myService
+ */
 module.exports = {
+  /**
+ * @name myService.sayAge
+ * @param {number} age - The age
+ * @description Echo the age provided
+ */
   sayAge,
+  /**
+   * Echo the name provided
+   * @param {string} age - The name
+   * @name myService.sayName
+   */
   sayName,
 };
